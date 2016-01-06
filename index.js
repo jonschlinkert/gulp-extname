55d59abb22380ce382934c6c24595ce16a9987e3
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-06 10:11:15 -0500

    use `file.extname` if it exists

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ var through = require('through2');
 var rewrite = require('rewrite-ext');
 
 module.exports = function(ext) {
-  if (typeof ext === 'object' && typeof ext != null) {
+  if (ext && typeof ext === 'object') {
     ext = ext.ext ? ext.ext : null;
   }
 
-  return through.obj(function (file, enc, cb) {
-    file.path = rewrite(file.path, ext);
-    this.push(file);
-    cb();
+  return through.obj(function(file, enc, next) {
+    if (ext && file.extname) {
+      file.extname = ext;
+    } else {
+      file.path = rewrite(file.path, ext);
+    }
+    next(null, file);
   });
 };
